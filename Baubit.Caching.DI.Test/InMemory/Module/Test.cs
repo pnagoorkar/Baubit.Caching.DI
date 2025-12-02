d00ed05edeb2ba375654a95e491689e3a0428fae
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-12-02 18:10:49 -0500

    Adding unit tests

---
+++ Baubit.Caching.DI.Test/InMemory/Module/Test.cs
@@ -0,0 +1,37 @@
+﻿using Baubit.DI;
+using Baubit.DI.Extensions;
+
+namespace Baubit.Caching.DI.Test.InMemory.Module
+{
+    /// <summary>
+    /// Unit tests for <see cref="DI.InMemory.Module{TValue}"/>
+    /// </summary>
+    public class Test
+    {
+        public class TestContext
+        {
+            public IOrderedCache<string> OrderedCache { get; init; }
+            public TestContext(IOrderedCache<string> orderedCache)
+            {
+                OrderedCache = orderedCache;
+            }
+        }
+        // rename the test method appropriately
+        public void Works()
+        {
+            var componentBuildResult = ComponentBuilder.CreateNew()
+                                                       .WithModule<DI.InMemory.Module<string>, DI.InMemory.Configuration>(BuildConfiguration)
+                                                       .Build()
+                                                       .Build<TestContext>();
+
+            Assert.True(componentBuildResult.IsSuccess);
+            Assert.NotNull(componentBuildResult.Value);
+            Assert.NotNull(componentBuildResult.Value.OrderedCache);
+        }
+
+        private void BuildConfiguration(DI.InMemory.Configuration configuration)
+        {
+
+        }
+    }
+}
